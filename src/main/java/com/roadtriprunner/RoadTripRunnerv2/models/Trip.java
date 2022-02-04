31d673b67280cdb84142958a502b989a5957d164
krystalwhite <krystalsnowhite@gmail.com>
2022-02-04 16:02:19 -0600

    added links to Java Client classes we might use instead of basic datatypes

--- src/main/java/com/roadtriprunner/RoadTripRunnerv2/models/Trip.java
+++ src/main/java/com/roadtriprunner/RoadTripRunnerv2/models/Trip.java
@@ -2,6 +2,27 @@ package com.roadtriprunner.RoadTripRunnerv2.models;
 
 public class Trip extends AbstractEntity {
 
+    /*
+    Question: Should tripName, startingLocation and endingLocation be of a different datatype?
+    See AddressComponent
+    https://www.javadoc.io/doc/com.google.maps/google-maps-services/latest/com/google/maps/model/AddressComponent.html
+
+    AutocompletePrediction and its relatives
+    https://www.javadoc.io/doc/com.google.maps/google-maps-services/latest/com/google/maps/model/AutocompletePrediction.html
+
+    FindPlaceByText
+    https://www.javadoc.io/doc/com.google.maps/google-maps-services/latest/com/google/maps/model/FindPlaceFromText.html
+
+    LatLng
+    https://www.javadoc.io/doc/com.google.maps/google-maps-services/latest/com/google/maps/model/LatLng.html
+
+    Geometry
+    https://www.javadoc.io/doc/com.google.maps/google-maps-services/latest/com/google/maps/model/Geometry.html
+
+    PlaceDetails and its relatives
+    https://www.javadoc.io/doc/com.google.maps/google-maps-services/latest/com/google/maps/model/PlaceDetails.html
+     */
+
     private String tripName;
 
     private String startingLocation;
